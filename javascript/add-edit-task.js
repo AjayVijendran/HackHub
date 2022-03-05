796ccd5a471a357852a60bf967da02e1f55f2fb7
GrandezXaferdian <95590221+GrandezXaferdian@users.noreply.github.com>
2022-03-05 13:37:39 +0530

    edit page js

--- javascript/add-edit-task.js
+++ javascript/add-edit-task.js
@@ -1,15 +1,44 @@
-const widget_tasknames = document.querySelector("#task-name");
-const widget_hours = document.querySelector("#hours");
+const widget_tasknames = document.querySelector(".task-name");
+const widget_hours = document.querySelector(".hours");
 
 
-let userdata = JSON.parse(localStorage.getItem("allTasks"));
+
 
 
 widget_tasknames.addEventListener("keyup",appendTask);
 widget_hours.addEventListener("keyup",appendTask);
 
+function displayTasks(data) {
+    htmlcode = "";
+    data.forEach((value,id) => {
+        htmlcode += `<div class = "task-widget"><p><span class = "display-hours">${value.time}</span> hours of <span class = "display-task"> ${value.name}</span></p>
+        <button id = ${id} class = "edit">Edit</button></div>`
+        
+    });
+
+    document.querySelector(".task-display").innerHTML = htmlcode;
+
+}
+
 
 function appendTask(eventObj) {
 
     let triggerObj = eventObj.currentTarget;
+    let taskObj = triggerObj.parentElement;
+    let hours = taskObj.querySelector(".hours").value.trim();
+    let task = taskObj.querySelector(".task-name").value.trim();
+
+    if (eventObj.key == 'Enter' && hours && task){
+        let userdata = JSON.parse(localStorage.getItem("allTasks"));
+        if (!userdata){
+            userdata = []; //Initialize empty list if no data is present
+        }
+
+        let taskInfo = {name: task, time: hours};
+        userdata.push(taskInfo);
+        localStorage.setItem('allTasks',JSON.stringify(userdata)); //update userdata
+
+        displayTasks(userdata)
+    }
+
 }
